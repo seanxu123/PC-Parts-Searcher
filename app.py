d4a01ea81df69aa2849d470e2b9fd1ec3070d738
seanxu123 <146236984+seanxu123@users.noreply.github.com>
2024-01-02 08:52:02 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,138 @@
+from flask import Flask, render_template, request
+import web_scraper
+import threading
+import json
+import os
+import time
+import datetime
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve environment variables
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+twilio_num = os.getenv("twilio.num")
+phone_num = os.getenv("phone_num")
+key = os.getenv("EBAY_API_KEY")
+
+# Global flags for search control
+is_new_search = False
+search_ongoing = False
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+     """Render the main index.html page."""
+     return render_template('index.html') 
+
+@app.route('/', methods=['POST'])
+def get_value():
+    """Handle form submission and initiate the search."""
+    global is_new_search
+    
+    # Collect form data for items and max prices
+    filled_elements = [v for k, v in request.form.items() if v] 
+  
+    items = []
+    max_prices = []
+    
+    # Split the collected data into items and max_prices lists
+    for index, value in enumerate(filled_elements):
+        if index % 2 == 0:
+            items.append(value)
+        else:
+            max_prices.append(value)
+    
+    #Function to send a notification message
+    def send_message(listing):
+        client = Client(account_sid, auth_token)
+        text = "New listing detected:\n" + listing["Title"] + "\n" + "Price: " + listing["Price"] + "\n" + listing["URL"]
+        
+        message = client.messages.create(
+            body = text,
+            from_=twilio_num,
+            to=phone_num
+        )
+        
+        print("Sent messsage:", message.body)        
+        
+    
+    # Continuous search function looking for listings every 30 seconds
+    def cont_search():
+        global is_new_search
+        global search_ongoing
+        
+        print("Continously searching...")
+        
+        search_ongoing = True
+        item_detected = False
+        checkpoint_time = datetime.datetime.now() 
+        
+        while is_new_search == False: #While the user hasn't submitted a new form        
+            time.sleep(30)
+            
+            for i in range(len(items)): 
+                print("Searching for", str(items))
+                listings = web_scraper.execute_search(key, items[i], max_prices[i])
+                
+                for listing in listings:
+                    listing_time = datetime.datetime.strptime(listing["Start Time"], "%Y-%m-%d %H:%M:%S")
+                    
+                    #If listing was posted later than checkpoint_point, notify user
+                    if listing_time >= checkpoint_time: 
+                        item_detected = True
+                        print("New listing detected!")
+                        print(listing)
+                        send_message(listing)
+                
+            if item_detected == False:
+                print("No new item detected")
+            else:
+                item_detected = False
+                checkpoint_time = datetime.datetime.now()
+
+            if is_new_search: #If user submitted new form, stop executing function
+                    search_ongoing = False
+                    break
+                
+             
+    try:
+       print('Returning results to front end')
+       total_results = []
+      
+       for i in range(len(items)):
+           item_results = web_scraper.execute_search(key, items[i], max_prices[i])
+           total_results.append(item_results)
+      
+       is_new_search = True
+       #Display search results in webpage
+       return render_template('tabs_opener.html', total_results=total_results, items = items)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return "Error occurred" 
+    
+    finally:
+         # Ensure previous search thread has completed before starting a new one
+        while search_ongoing: 
+            time.sleep(1)
+            
+        is_new_search = False
+        threading.Thread(target=cont_search).start() #Start background_search
+        
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
+    
+
+   
+    
+     
---
+++ web_scraper.py
@@ -0,0 +1,109 @@
+import os
+import pytz
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+key = os.getenv("EBAY_API_KEY")
+
+# eBay class for fetching listings
+class Ebay(object):
+    def __init__(self, key, search_term, max_price):
+        self.key = key
+        self.search_term = search_term
+        self.max_price = max_price
+       
+    
+    def fetch(self):
+        listings = [] # List to store fetched listings
+        item_count = 0 # Counter for items
+        
+        # Initialize eBay API connection
+        api = Connection(appid=self.key,  config_file=None, siteid ="EBAY-ENCA")
+        #You can fetch up to 100 pages of data according to the API documentation
+        MAX_PAGE = 100 
+        
+        try:
+            for page_num in range(1, MAX_PAGE + 1):
+                # Make API request to eBay
+                response = api.execute('findItemsAdvanced', {'keywords': self.search_term, 
+                                                            'paginationInput': {'pageNumber': page_num}, 
+                                                            "itemFilter": [
+                                                                {
+                                                                  "name": "MaxPrice",
+                                                                  "value": self.max_price,
+                                                                  "paramName": "Currency",
+                                                                  "paramValue": "CAD",
+                                                                }], 
+                                                            "outputSelector": ["sellerInfo", "shippingInfo", "condition"]
+                                                            })
+                
+
+                page_number = response.reply.paginationOutput.pageNumber 
+                print("Page Number:", page_number) #Print Page Number
+                
+                items = response.reply.searchResult.get('item', [])
+                #If no items are returned, or reached end of the list, return listings
+                if len(items) == 0 or page_num == 101:
+                    return listings 
+
+
+                #Function that checks for duplicate items (items with same title)
+                def is_duplicate(title):
+                    for item in listings:
+                        if item["Title"] == title:
+                            return True
+                         
+                    return False
+                
+                
+                # Process each item in the response
+                for item in items:
+                    if hasattr(response.reply.searchResult, 'item'):
+                      
+                        price = item.sellingStatus.convertedCurrentPrice.value
+                        shipping_fee = item.shippingInfo.shippingServiceCost.value if hasattr(item.shippingInfo, "shippingServiceCost") else None
+                        title = item.title
+                        url = item.viewItemURL
+                        gmt_start_time = item.listingInfo.startTime 
+                        gmt_start_time = pytz.timezone('Etc/GMT').localize(gmt_start_time) #Localize time to GMT
+                        atl_start_time = gmt_start_time.astimezone(pytz.timezone('America/Toronto')) #Convert time at EST
+                        atl_start_time = atl_start_time.strftime('%Y-%m-%d %H:%M:%S') #Convert time to string
+                        condition = item.condition.conditionDisplayName if hasattr(item, 'condition') else None
+                            
+                        # Check condition and duplicate status before adding to listings
+                        if condition != "For parts or not working" and is_duplicate(title) == False:
+                            item_count += 1
+                
+                            attributes = {
+                                "Item count": item_count,
+                                "Title": title,
+                                'Price': price,
+                                'Shipping fee': shipping_fee,
+                                'URL': url,
+                                'Condition': condition,
+                                'Start Time': atl_start_time
+                            }
+                            listings.append(attributes)          
+            
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
+
+
+# Function to execute the eBay search
+def execute_search(key, items, max_prices):
+    return Ebay(key, items, max_prices).fetch()
+    
+
+
+    
+
+
+
+
+
+
